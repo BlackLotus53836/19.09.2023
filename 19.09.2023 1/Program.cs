d13f6dab6a0ab2a067d71144bfa20fd178af4855
Mark <Loop>
2023-09-19 18:59:38 +0300

    Добавьте файлы проекта.

---
+++ 19.09.2023 1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите значение M: ");
+        int m = int.Parse(Console.ReadLine());
+
+        Console.Write("Введите значение N: ");
+        int n = int.Parse(Console.ReadLine());
+
+        int sum = 0;
+        for (int i = m; i <= n; i++)
+        {
+            sum += i;
+        }
+
+        Console.WriteLine("Сумма натуральных элементов в промежутке от M до N: " + sum);
+    }
+}
